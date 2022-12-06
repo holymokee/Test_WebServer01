1bd08fd2ee70ad2c9a767a4568ad173b89eb1bca
holymokee <holymokee@gmail.com>
2022-12-06 17:31:03 +0900

    2022.12.16_회사

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import random
 
 app = Flask(__name__)
 
+nextID = 4
 topics = [ 
     {'id': 1, 'title' : 'html', 'body' : 'html is...'},
     {'id': 2, 'title' : 'css', 'body' : 'css is...'},
     {'id': 3, 'title' : 'javascript', 'body' : 'javascript is...'},
 ]
 
-def SetHtml(contents, context):
+def SetHtml(contents, context, id = None):
     return f'''<!doctype html>
     <html>
         <body>
@@ -18,6 +19,9 @@ def SetHtml(contents, context):
                 {contents}
             </ol>
             {context}
+            <ul>
+                <li><a href="/create/">Create</a></li>
+            </ul>    
         </body>
     </html>
     '''
@@ -35,14 +39,37 @@ def index():
 
     return SetHtml(GetTags(), f'<h2>Welcome</h2>Hello, Web')
     
-@app.route('/create/')
+@app.route('/create/', methods = ['GET', 'POST'])
 def index2():
-    return 'Create 03'
+    global nextID
+
+    if request.method == 'GET':
+        content = '''
+            <form action="/create/" method = "POST">
+                <p><input type = "text" name = "title" placeholder = "title"></p>
+                <p><textarea name = "body" placeholder = "body"></textarea></p>
+                <p><input type = "submit" value="create"></P>
+                <p><input type = "submit" value="update"></P>
+            </form>
+        '''
+        return SetHtml(GetTags(), content)
+
+    elif request.method == 'POST':     
+        title = request.form['title']
+        body = request.form['body']
+
+        newTopic = {'id' : nextID , 'title' : title, 'body' : body}
+        url = '/read/' + str(nextID) + '/'
+        nextID += 1
+
+        topics.append(newTopic)
+
+        return redirect(url)
+
 
 @app.route('/read/<int:id>/')
 def read(id):
 
-    title = ''
     body = ''
     
     for topic in topics:
@@ -51,6 +78,6 @@ def read(id):
             body = topic["body"]
             break
 
-    return SetHtml(GetTags(), f'<h2>{title}</h2>{body}')
+    return SetHtml(GetTags(), f'<h2>{title}</h2>{body}', id)
 
 app.run(debug=True)
